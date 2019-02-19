ec9f7f4ee1d4d0d26281a2c58f3ea8faf36ac275
ACSD <dev.acsd@gmail.com>
2019-02-18 20:14:18 -0800

    Update hexgrids.js

--- hex-grid/JavaScript/hexgrids.js
+++ hex-grid/JavaScript/hexgrids.js
@@ -1,9 +1,14 @@
-var container, stats, info, camera, scene, renderer, group, hg;
-var target_heights = new Map(), materials=[];
+var container, stats, info, camera, scene, renderer, group, hg, sel;
+var gui, mat = { color: 0x696969, specular: 0x000000, shininess: 30, flatShading: true };
 
-var size=20, step = 2 ** -8, select_count=8;
+var config = { size:31, step: 2 ** -8, select_count: 8 };
+var app = { grid: new THREE.HexGrid() };
 
-init();window.addEventListener("load",function(){animate(); generate_grid();});
+var target_heights = new Map();
+
+var select_count=8;
+
+init();window.addEventListener("load",function(){animate(); generate_grid(); create_gui();});
 
 function init() {
     container = document.getElementById( 'container' );
@@ -17,29 +22,13 @@ function init() {
     var light=new THREE.PointLight(0xffffff,1);camera.add(light);
     camera.add(new THREE.AmbientLight( 0x111111 ));
 
-    // materials.push( new THREE.MeshLambertMaterial( { map: texture, transparent: true } ) );
-    materials.push( new THREE.MeshLambertMaterial( { color: 0x696969 } ) );
-    materials.push( new THREE.MeshPhongMaterial( { color: 0x696969, specular: 0x000000, shininess: 30, flatShading: true } ) );
-    materials.push( new THREE.MeshNormalMaterial() );
-    materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending } ) );
-    materials.push( new THREE.MeshLambertMaterial( { color: 0xdddddd } ) );
-    // materials.push( new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, map: texture, transparent: true } ) );
-    materials.push( new THREE.MeshNormalMaterial( { flatShading: true } ) );
-    materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, wireframe: true } ) );
-    materials.push( new THREE.MeshDepthMaterial() );
-    materials.push( new THREE.MeshLambertMaterial( { color: 0x666666, emissive: 0xff0000 } ) );
-    materials.push( new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0x666666, emissive: 0xff0000, shininess: 10, opacity: 0.9, transparent: true } ) );
-    // materials.push( new THREE.MeshBasicMaterial( { map: texture, transparent: true } ) );
-
-    group=new THREE.Group();group.position.y=50;scene.add(group);
-
-
+    group=new THREE.Group();group.position.y=window.innerHeight/16;scene.add(group);
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
-    window.addEventListener( 'mousedown', generate_grid, false );
+    // window.addEventListener( 'mousedown', generate_grid, false );
     window.addEventListener( 'resize', onWindowResize, false );
 }
 
@@ -49,22 +38,26 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     generate_grid();
 }
-function rc(){return 0x111111;}
-// function rc(){ return (Math.random()*0xFFFFFF)<<0; }
 function hc(c,s,i){var d=60*i;var a=Math.PI/180*d;return {x:(c.x+s*Math.cos(a)),y:(c.y+s*Math.sin(a))};}
 
 function generate_grid(){
-    var p;hg=new THREE.Shape();for(var i=0;i<6;i++){p=hc({x:0,y:0},size,i+1);i==0?hg.moveTo(p.x,p.y):hg.lineTo(p.x,p.y);}
+    scene.background=new THREE.Color(mat.color);
+    var p;hg=new THREE.Shape();for(var i=0;i<6;i++){p=hc({x:0,y:0},config.size,i+1);i==0?hg.moveTo(p.x,p.y):hg.lineTo(p.x,p.y);}
 
     while(group.children.length>0){group.remove(group.children[0]);}
-    var e={depth:size*4,bevelEnabled:true,bevelSegments:2,steps:2,bevelSize:1,bevelThickness:1};
+    var e={depth:100,bevelEnabled:true,bevelSegments:2,steps:2,bevelSize:1,bevelThickness:1};
 
-    var w=(2*size)*0.76,h=Math.sqrt(Math.PI)*size;
-    var c=((window.innerWidth/(w*0.8))<<0),r=((window.innerHeight/(h*1.4))<<0)+9;
-    var a=(((c)*w)/2),b=((r*h)/1.8);
+    var w=(2*config.size)*0.76,h=Math.sqrt(Math.PI)*config.size;
+    var c=(((window.innerWidth*1.1)/(w*4))<<0)+(15-(config.size / 10)),
+        r=(((window.innerHeight*1.1)/(h*4))<<0)+(14-(config.size / 10));
+    var a=((c*w)/2),b=((r*h)/1.8);
 
-    for(var x=0;x<c;x++){for(var y=0;y<r;y++){as(hg,e,rc(),(x*w)-a,((y*h)-(x%2?h/2:0))-b,0,0,0,0,1);}}
+    for(var x=0;x<c;x++){for(var y=0;y<r;y++){as(hg,e,0x111111,(x*w)-a,((y*h)-(x%2?h/2:0))-b,0,0,0,0,1);}}
     //group.traverse(function(e){e.scale.set(1,1,Math.abs(Math.random()));});
+
+    if(app.gui)app.gui.sel_.max(group.children.length);
+    group.position.x = 0.5*config.size;
+    group.position.y = (Math.sqrt(Math.PI/2)*config.size);
 }
 
 function animate(){requestAnimationFrame(animate);render();}
@@ -73,14 +66,15 @@ function render(){group.traverse(ht);sc();renderer.render(scene,camera);}
 function as( shape, extrudeSettings, color, x, y, z, rx, ry, rz, s ) {
     // extruded shape
     var geometry = new THREE.ExtrudeBufferGeometry( shape, extrudeSettings );
-    var material = new THREE.MeshPhongMaterial({color:color,specular:0x666666,emissive:0x888888,shininess:-10});
-    var mesh = new THREE.Mesh( geometry, materials[1] );
+    var material = new THREE.MeshPhongMaterial(mat);
+
+    var mesh = new THREE.Mesh( geometry, material );
     mesh.position.set(x,y,z);mesh.rotation.set(rx,ry,rz);mesh.scale.set(s,s,s);group.add(mesh);
 }
 
 function sc() {
     if(!group.children)return;var items=group.children,probability=0.1;
-    for(var i=0; i < select_count; i++) {
+    for(var i=0; i < config.select_count; i++) {
         var item = items[Math.floor(Math.random()*items.length)];if(!item)return;
         if(Math.random()>(1-probability) && !target_heights.has(item.uuid))
             target_heights.set(item.uuid,Math.abs(Math.random()));
@@ -91,8 +85,21 @@ function ht(child) {
     if(target_heights.has(child.uuid)) {
         var c = child.scale.z;
         var t=target_heights.get(child.uuid);
-        var d=t-c;var m=(d%step);
+        var d=t-c;var m=(d%config.step);
         if(c==t-m)target_heights.delete(child.uuid);
-        else c-=d>0?-step:step;child.scale.set(1,1,c);
+        else c-=d>0?-config.step:config.step;child.scale.set(1,1,c);
     }
+}
+
+function create_gui() {
+    if(app.gui) app.gui.remove();
+    app.gui = new dat.GUI( { name: "Hex Grid", width: 350, hideable: true } );
+
+    app.gui.color = app.gui.addColor(mat, "color").onChange(generate_grid);
+
+    app.gui.size = app.gui.add(config, "size", 20, 100, 1).onChange(generate_grid);
+    app.gui.step = app.gui.add(config, "step", 0, 0.05, 2**-8);
+    app.gui.sel_ = app.gui.add(config, "select_count", 1, 100, 1);
+
+    // app.gui.add(app.grid,'generate').name("Generate");
 }
